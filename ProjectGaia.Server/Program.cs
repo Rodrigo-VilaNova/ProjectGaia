3201420d8a601436e03c3c961399e250aa225409
Rodrigo-VilaNova <202200196@estudantes.ips.pt>
2025-03-23 19:54:54 +0000

    Correção para o Azure

--- ProjectGaia.Server/Program.cs
+++ ProjectGaia.Server/Program.cs
@@ -35,7 +35,7 @@ namespace ProjectGaia.Server
             {
                 options.AddPolicy("AllowSpecificOrigin", policy =>
                 {
-                    if (builder.Environment.IsProduction()) policy.WithOrigins(["https://gaia.pombos.net:443"]);
+                    if (builder.Environment.IsProduction()) policy.WithOrigins(["https://projectgaia.azurewebsites.net", "https://gaia.pombos.net:443"]);
                     else policy.WithOrigins(["http://127.0.0.1:5002", "http://localhost:5002"]);
 
                     policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials();
