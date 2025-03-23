92d288e10ad96ffa378f409b975c04895d041ff2
Rodrigo-VilaNova <202200196@estudantes.ips.pt>
2025-03-23 22:02:30 +0000

    Configuração para Azure

---
+++ projectgaia.client/src/environments/environment.prod-pombos.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://gaia.pombos.net/api'
+};
--- projectgaia.client/src/environments/environment.prod.ts
+++ projectgaia.client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://gaia.pombos.net/api'
+  apiUrl: 'https://projectgaia.azurewebsites.net/api'
 };
